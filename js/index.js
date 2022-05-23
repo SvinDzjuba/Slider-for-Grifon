4d764336c01295d8668c45b1455e372ab0f349a9
Максим Дзюбенко <maksim.dzjubenko@ivkhk.ee>
2022-05-23 23:29:04 +0300

    pretty good, but have to make arrows or smth

--- js/index.js
+++ js/index.js
@@ -2,23 +2,34 @@ const firstTmp = document.getElementById('firstTmp');
 const secondTmp = document.getElementById('secondTmp');
 
 firstTmp.addEventListener('click', (e) => {
-    secondTmp.style.transition = "ease all 0.8s"
-    secondTmp.style.transform = "translateX(-100%)"
+    setTimeout(() => secondTmp.style.opacity = "1", 1);
+    firstTmp.style.pointerEvents = "none";
+    secondTmp.style.pointerEvents = "none";
+    secondTmp.style.transition = "ease all 0.4s";
     firstTmp.style.transform = "translateX(100%)";
+    setTimeout(() => firstTmp.style.opacity = "0", 1);
     secondTmp.style.transform = "translateX(0%)";
     setTimeout(() => {
         Promise.resolve()
             .then(() => firstTmp.style.transition = "none")
-            .then(() => firstTmp.style.transform = "translateX(-100%)");
-    }, 750);
+            .then(() => firstTmp.style.transform = "translateX(-100%)")
+            .then(() => firstTmp.style.pointerEvents = "all")
+            .then(() => secondTmp.style.pointerEvents = "all")
+    }, 350);
 });
 secondTmp.addEventListener('click', (e) => {
-    firstTmp.style.transition = "ease all 0.8s"
+    setTimeout(() => firstTmp.style.opacity = "1", 1);
+    firstTmp.style.pointerEvents = "none";
+    secondTmp.style.pointerEvents = "none";
+    firstTmp.style.transition = "ease all 0.4s"
     secondTmp.style.transform = "translateX(100%)";
+    setTimeout(() => secondTmp.style.opacity = "0", 1);
     firstTmp.style.transform = "translateX(0%)";
     setTimeout(() => {
         Promise.resolve()
             .then(() => secondTmp.style.transition = "none")
-            .then(() => secondTmp.style.transform = "translateX(-100%)");
-    }, 750);
+            .then(() => secondTmp.style.transform = "translateX(-100%)")
+            .then(() => firstTmp.style.pointerEvents = "all")
+            .then(() => secondTmp.style.pointerEvents = "all")
+    }, 350);
 });
